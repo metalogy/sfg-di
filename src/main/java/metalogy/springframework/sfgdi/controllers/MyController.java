10bccddb4f32705529561d9d483e80f2415ed61f
metalogy <metalogy@wp.pl>
2022-03-21 19:40:31 +0100

    Di

---
+++ src/main/java/metalogy/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,11 @@
+package metalogy.springframework.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello(){
+        System.out.println("Hello");
+        return "Hi folks";
+    }
+}
