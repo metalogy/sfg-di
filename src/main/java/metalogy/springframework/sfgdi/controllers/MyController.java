75c5d7079dfd98479468b747800d62c10ed6d907
metalogy <metalogy@wp.pl>
2022-03-23 23:40:17 +0100

    Primary bean

--- src/main/java/metalogy/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/metalogy/springframework/sfgdi/controllers/MyController.java
@@ -1,11 +1,18 @@
 package metalogy.springframework.sfgdi.controllers;
 
+import metalogy.springframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
-    public String sayHello(){
-        System.out.println("Hello");
-        return "Hi folks";
+
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
     }
 }
