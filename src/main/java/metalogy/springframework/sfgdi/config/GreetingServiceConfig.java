2ed4746ce3d8c225c81ce0093b72db864187096e
metalogy <metalogy@wp.pl>
2022-04-19 23:46:34 +0200

    Java config

---
+++ src/main/java/metalogy/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,20 @@
+package metalogy.springframework.sfgdi.config;
+
+import metalogy.springframework.sfgdi.services.ConstructorGreetingService;
+import metalogy.springframework.sfgdi.services.PropertyInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService() {
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService() {
+        return new PropertyInjectedGreetingService();
+    }
+}
